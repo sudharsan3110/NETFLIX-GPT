e3b583a77920adbf86e1037b8053e7aafa876e15
SUDHAARSAN <m.sudhar007@gmail.com>
2024-07-09 12:13:25 +0530

    initial repo created

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+function App() {
+  return <div className=" text-gray-500">
+    <h1 className='text-3xl'>Nameste evertyone let built nameste netflix gpt </h1>
+  </div>
+}
+
+export default App
